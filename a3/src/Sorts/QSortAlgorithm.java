989aa10c23e15d784a4a554d9ff6b535eff73589
Johnson <johnson7267@gmail.com>
2022-03-30 16:15:35 -0400

    Reverted Changes

--- a3/src/Sorts/QSortAlgorithm.java
+++ a3/src/Sorts/QSortAlgorithm.java
@@ -53,81 +53,79 @@ package Sorts;
  */
 
 public class QSortAlgorithm extends SortAlgorithm {
-    /* This function takes last element as pivot,
-    places the pivot element at its correct
-    position in sorted array, and places all
-    smaller (smaller than pivot) to left of
-    pivot and all greater elements to right
-    of pivot */
-    static int partition(int[] arr, int low, int high)
-    {
-        int pivot = arr[high];
- 
-        // index of smaller element
-        int i = (low - 1);
-        for (int j = low; j <= high - 1; j++) {
-            // If current element is smaller than or
-            // equal to pivot
-            if (arr[j] <= pivot) {
-                i++;
- 
-                // swap arr[i] and arr[j]
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+    static void sort(int[] a, int lo0, int hi0)  {
+        int lo = lo0;
+        int hi = hi0;
+
+        if (lo >= hi) {
+            return;
         }
- 
-        // swap arr[i+1] and arr[high] (or pivot)
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
- 
-        return i + 1;
-    }
- 
-    /* A[] --> Array to be sorted,
-   l  --> Starting index,
-   h  --> Ending index */
-    static void quickSortIterative(int[] arr, int l, int h)
-    {
-        // Create an auxiliary stack
-        int[] stack = new int[h - l + 1];
- 
-        // initialize top of stack
-        int top = -1;
- 
-        // push initial values of l and h to stack
-        stack[++top] = l;
-        stack[++top] = h;
- 
-        // Keep popping from stack while is not empty
-        while (top >= 0) {
-            // Pop h and l
-            h = stack[top--];
-            l = stack[top--];
- 
-            // Set pivot element at its correct position
-            // in sorted array
-            int p = partition(arr, l, h);
- 
-            // If there are elements on left side of pivot,
-            // then push left side to stack
-            if (p - 1 > l) {
-                stack[++top] = l;
-                stack[++top] = p - 1;
+        else if( lo == hi - 1 ) {
+            /*
+             *  sort a two element list by swapping if necessary
+             */
+            if (a[lo] > a[hi]) {
+                int T = a[lo];
+                a[lo] = a[hi];
+                a[hi] = T;
             }
- 
-            // If there are elements on right side of pivot,
-            // then push right side to stack
-            if (p + 1 < h) {
-                stack[++top] = p + 1;
-                stack[++top] = h;
-            }
+            return;
         }
+
+
+        /*
+         *  Pick a pivot and move it out of the way
+         */
+        int pivot = a[(lo + hi) / 2];
+        a[(lo + hi) / 2] = a[hi];
+        a[hi] = pivot;
+
+        while( lo < hi ) {
+            /*
+             *  Search forward from a[lo] until an element is found that
+             *  is greater than the pivot or lo >= hi
+             */
+            while (a[lo] <= pivot && lo < hi) {
+                lo++;
+            }
+
+            /*
+             *  Search backward from a[hi] until element is found that
+             *  is less than the pivot, or lo >= hi
+             */
+            while (pivot <= a[hi] && lo < hi ) {
+                hi--;
+            }
+
+            /*
+             *  Swap elements a[lo] and a[hi]
+             */
+            if( lo < hi ) {
+                int T = a[lo];
+                a[lo] = a[hi];
+                a[hi] = T;
+
+            }
+
+
+        }
+
+        /*
+         *  Put the median in the "center" of the list
+         */
+        a[hi0] = a[hi];
+        a[hi] = pivot;
+
+        /*
+         *  Recursive calls, elements a[lo0] to a[lo-1] are less than or
+         *  equal to pivot, elements a[hi+1] to a[hi0] are greater than
+         *  pivot.
+         */
+        sort(a, lo0, lo-1);
+        sort(a, hi+1, hi0);
     }
 
     public static void sort(int[] a) {
-        quickSortIterative(a, 0, a.length-1);
+        sort(a, 0, a.length-1);
     }
 }
